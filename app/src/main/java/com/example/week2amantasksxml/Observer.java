d100542207e028ca0b4fcaf1a43588d1be0693f2
eyadyehia <eyadyehia07@gmail.com>
2026-07-15 10:04:55 +0300

    feat: Observer using Java
    Added observer pattern so that any collector subscribes to it can be notified with changes

---
+++ app/src/main/java/com/example/week2amantasksxml/Observer.java
@@ -0,0 +1,12 @@
+package com.example.week2amantasksxml;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Observer {
+
+    public void onValueChanged(int value) {
+        System.out.println("Value changed to " + value);
+    }
+}
+
